5341ea854d3d65d90f333289fd437f3583d437be
hieunguyen <hieunguyen@vnpt.vn>
2025-11-02 15:22:27 +0700

    oke

--- cashbee-presentation/src/main/java/com/cashbee/presentation/config/SecurityConfig.java
+++ cashbee-presentation/src/main/java/com/cashbee/presentation/config/SecurityConfig.java
@@ -138,7 +138,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3007",
                 "https://cashbee.nguocchieuvangle.io.vn/", "https://video.management.v1.openlearnhub.io.vn/",
-                "https://smart.feeds.openlearnhub.io.vn/"));
+                "https://auth.nguocchieuvangle.io.vn/"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "X-Requested-With"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(Arrays.asList(
